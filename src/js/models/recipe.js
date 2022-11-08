7f55eb09c8ae2c6f238e00259303f28a28e6b5ef
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-11-08 19:35:03 -0300

    update Model && Template recipes

--- src/js/models/recipe.js
+++ src/js/models/recipe.js
@@ -16,7 +16,38 @@ export default class RecipeModel {
 
   get description() { return this._description; }
 
-  get ingredients() { return this._ingredients; }
+  get ingredients() {
+    let ingredientItem = "";
+
+    for (let data of this._ingredients) {
+      const ingredient = data.ingredient;
+      const quantity = (data.quantity !== undefined) ? `:</span> ${data.quantity}` : "</span>";
+      let unit = "";
+      if (data.unit !== undefined) {
+        if (data.unit === "ml") { unit = "ml" }
+        if (data.unit === "cl") { unit = "cl" }
+        if (data.unit === "litres") { unit = "l" }
+        if (data.unit === "grammes") { unit = "g" }
+        if (data.unit === "kg") { unit = "kg" }
+        if (data.unit === "cuillères à café") { unit = "càc" }
+        if (data.unit === "cuillères à soupe") { unit = " cuillères" }
+        if (data.unit === "sachets") { unit = " sachets" }
+        if (data.unit === "tranches") { unit = " tranches" }
+        if (data.unit === "tasses") { unit = " tasses" }
+        if (data.unit === "tiges") { unit = " tiges" }
+        if (data.unit === "gousses") { unit = " gousses" }
+        if (data.unit === "verres") { unit = " verres" }
+        if (data.unit === "boites") { unit = " boîtes" }
+        if (data.unit === "barquettes") { unit = " barquettes" }
+        if (data.unit === "pincées") { unit = " pincées" }
+        if (data.unit === "feuilles") { unit = " feuilles" }
+      } else { unit = "" }
+  
+      ingredientItem += `<li><span class="fw-bold">${ingredient}${quantity}${unit}</li>`
+    }
+
+    return ingredientItem;
+  }
 
   get appliance() { return this._appliance; }
 
