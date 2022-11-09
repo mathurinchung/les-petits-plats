fcb786f49005379400859a3a62788146fd2e1570
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-11-09 17:30:14 -0300

    update subject

--- js/factories/recipe.js
+++ js/factories/recipe.js
@@ -12,6 +12,7 @@ export default class RecipeFactory {
     const ingredient = item.ingredient;
     const quantity = (item.quantity !== undefined) ? `:</span> ${item.quantity}` : "</span>";
     let unit = "";
+
     switch (item.unit) {
     case "ml": unit = "ml";
       break;
