238c70f30bf442219e184084fb1763568ce3e29c
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-11-09 16:22:44 -0300

    update recipecard

--- js/templates/recipe.js
+++ js/templates/recipe.js
@@ -1,7 +1,24 @@
 export default class RecipeTemplate {
-  constructor(data) {
+  constructor(data, ingredients) {
     this._data = data;
+    this._ingredients = ingredients;
   }
 
-  RecipeCardDOM() {}
+  RecipeCardDOM() {
+    return `
+      <article class="card" data-id="${this._data.id}">
+        <figure class="card-img-top"></figure>
+        <div class="card-body d-grid">
+          <div class="card-heading d-flex justify-content-between align-items-start">
+            <span class="card-name">${this._data.name}</span>
+            <span class="card-time d-flex align-items-center fw-bold">
+              <i class="icon icon-clock" aria-hidden="true"></i> ${this._data.time} min
+            </span>
+          </div>
+          <ul class="card-ingredients">${this._ingredients}</ul>
+          <p class="card-description">${this._data.description}</p>
+        </div>
+      </article>
+    `;
+  }
 }
