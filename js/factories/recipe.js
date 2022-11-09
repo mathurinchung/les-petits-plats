fd7cc87dbef656b7d1bb3a4a98031e594be9b800
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-11-09 19:10:04 -0300

    fix name type

--- js/factories/recipe.js
+++ js/factories/recipe.js
@@ -6,7 +6,7 @@ export default class RecipeFactory {
   constructor(data, type) {
     this._data = new RecipeModel(data);
 
-    if (type === "page") return new RecipeTemplate(this._data);
+    if (type === "card") return new RecipeTemplate(this._data);
     if (type === "components") return new ComponentsUtils(this._data);
   }
 
