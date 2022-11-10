3146d6c6bc9ba7f28722915bec2603429457d617
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-11-10 06:24:58 -0300

    delete components

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,6 +1,7 @@
 import RecipeService from "../services/recipe.js";
 // import CardFactory from "../factories/card.js";
 // import DropdownFactory from "../factories/dropdown.js";
+import ComponentsUtils from "../utils/components.js";
 
 class App {
   displayCard(recipes) {
@@ -16,11 +17,18 @@ class App {
     dropdownContainer.innerHTML = ""; // display dropdown element
   }
 
+  handleComponents(recipes) {
+    const componentsUtils = new ComponentsUtils(recipes);
+
+    componentsUtils.handler();
+  }
+
   init() {
     const recipes = RecipeService.getAllRecipe();
 
     this.displayCard(recipes);
     this.displayDropdown(recipes);
+    this.handleComponents(recipes);
   }
 }
 
