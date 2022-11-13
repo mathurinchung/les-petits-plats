7d77b732615f08bf124f443f7828ea76309350c3
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-11-12 22:00:16 -0300

    update dropdown

--- js/factories/dropdown.js
+++ js/factories/dropdown.js
@@ -1,23 +1,26 @@
-import RecipeModel from "../models/recipe.js";
+import RecipeFactory from "./recipe.js";
 import DropdownTemplate from "../templates/dropdown.js";
 
 export default class DropdownFactory {
   constructor(data, type) {
-    this._data = data;
+    this._data = new RecipeFactory(data);
     this.type = type;
 
     if (this.type === "ingredients") {
       this.textContent = "Ingredients";
-      this.list = this.#DropdownIngredientsList();
+      this.placeholder = "ingrédient";
+      this.list = this._data.IngredientsList();
     } else if (this.type === "appliances") {
       this.textContent = "Appareils";
-      this.list = this.#DropdownAppliancesList();
+      this.placeholder = "appareil";
+      this.list = this._data.AppliancesList();
     } else if (this.type === "ustensils") {
       this.textContent = "Ustensiles";
-      this.list = this.#DropdownUstensilsList();
+      this.placeholder = "ustensile";
+      this.list = this._data.UstensilsList();
     }
 
-    return new DropdownTemplate(this.type, this.textContent, this.#DropdownList());
+    return new DropdownTemplate(this.type, this.textContent, this.placeholder, this.#DropdownList());
   }
 
   #DropdownList() {
@@ -28,42 +31,5 @@ export default class DropdownFactory {
     }
 
     return dropdownItem;
-  }
-
-  #DropdownIngredientsList() {
-    const arr = new Set();
-
-    for (let item of this._data) {
-      const recipe = new RecipeModel(item);
-      for (let item of recipe.ingredients) {
-        arr.add(item.ingredient);
-      }
-    }
-
-    return [ ...arr ];
-  }
-
-  #DropdownAppliancesList() {
-    const arr = new Set();
-
-    for (let item of this._data) {
-      const recipe = new RecipeModel(item);
-      arr.add(recipe.appliance);
-    }
-
-    return [ ...arr ];
-  }
-
-  #DropdownUstensilsList() {
-    const arr = new Set();
-
-    for (let item of this._data) {
-      const recipe = new RecipeModel(item);
-      for (let item of recipe.ustensils) {
-        arr.add(item);
-      }
-    }
-
-    return [ ...arr ];
   }
 }
