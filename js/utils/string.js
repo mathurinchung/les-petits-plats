eb9d75613d98644842eac3908a981e6a1741460a
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-12-02 19:23:46 -0300

    update string utils

--- js/utils/string.js
+++ js/utils/string.js
@@ -1,9 +1,8 @@
 export default class StringUtils {
   static formatText(string) {
-    return string.toLowerCase()
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .replace(/\s/g,"");
+    return string.toLowerCase() // lower case
+      .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // remove accents
+      .replace(/\s/g,""); // remove spaces
   }
 
   static setString(item) { return item.charAt(0).toUpperCase() + item.slice(1).toLowerCase(); }
