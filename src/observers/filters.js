3bad0272a978d075f750c89fa0118975e5e1660d
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2023-06-11 13:09:34 +0000

    fix filter observer

--- src/observers/filters.js
+++ src/observers/filters.js
@@ -10,7 +10,7 @@ export default class FiltersObserver {
 
     document.body.addEventListener('click', () => {
       filterElements.forEach(element => {
-        const notFilterElements = [ ...this.filterElements ].filter(el => el !== element);
+        const notFilterElements = [ ...filterElements ].filter(el => el !== element);
         this.filterContainer.handleCloseFilter(notFilterElements);
       });
     });
